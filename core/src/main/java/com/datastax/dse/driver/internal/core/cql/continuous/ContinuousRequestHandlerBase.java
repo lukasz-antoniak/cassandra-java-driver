69b629c3f680a30e1bd1c58a12a574cf3a48650e
Andrew Weaver <Andrew.Weaver@wal-mart.com>
2023-09-18 13:22:57 -0500

    Check type for ExecutionInfo in ContinuousRequestHandlerBase.

--- core/src/main/java/com/datastax/dse/driver/internal/core/cql/continuous/ContinuousRequestHandlerBase.java
+++ core/src/main/java/com/datastax/dse/driver/internal/core/cql/continuous/ContinuousRequestHandlerBase.java
@@ -19,11 +19,13 @@ package com.datastax.dse.driver.internal.core.cql.continuous;
 
 import com.datastax.dse.driver.api.core.DseProtocolVersion;
 import com.datastax.dse.driver.api.core.cql.continuous.ContinuousAsyncResultSet;
+import com.datastax.dse.driver.api.core.graph.AsyncGraphResultSet;
 import com.datastax.dse.driver.internal.core.DseProtocolFeature;
 import com.datastax.dse.driver.internal.core.cql.DseConversions;
 import com.datastax.dse.protocol.internal.request.Revise;
 import com.datastax.dse.protocol.internal.response.result.DseRowsMetadata;
 import com.datastax.oss.driver.api.core.AllNodesFailedException;
+import com.datastax.oss.driver.api.core.AsyncPagingIterable;
 import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.DriverTimeoutException;
 import com.datastax.oss.driver.api.core.NodeUnavailableException;
@@ -490,9 +492,6 @@ public abstract class ContinuousRequestHandlerBase<StatementT extends Request, R
 
     // Coordinates concurrent accesses between the client and I/O threads
     private final ReentrantLock lock = new ReentrantLock();
-
-    // The execution info for passing to the request tracker.
-    private ExecutionInfo executionInfo;
 
     // The page queue, storing responses that we have received and have not been consumed by the
     // client yet. We instantiate it lazily to avoid unnecessary allocation; this is also used to
@@ -1565,6 +1564,14 @@ public abstract class ContinuousRequestHandlerBase<StatementT extends Request, R
       if (resultSetClass.isInstance(pageOrError)) {
         if (future.complete(resultSetClass.cast(pageOrError))) {
           throttler.signalSuccess(ContinuousRequestHandlerBase.this);
+
+          ExecutionInfo executionInfo = null;
+          if (pageOrError instanceof AsyncPagingIterable) {
+            executionInfo = ((AsyncPagingIterable) pageOrError).getExecutionInfo();
+          } else if (pageOrError instanceof AsyncGraphResultSet) {
+            executionInfo = ((AsyncGraphResultSet) pageOrError).getRequestExecutionInfo();
+          }
+
           if (nodeSuccessReported.compareAndSet(false, true)) {
             context
                 .getRequestTracker()
@@ -1599,20 +1606,18 @@ public abstract class ContinuousRequestHandlerBase<StatementT extends Request, R
     private ExecutionInfo createExecutionInfo(@NonNull Result result, @Nullable Frame response) {
       ByteBuffer pagingState =
           result instanceof Rows ? ((Rows) result).getMetadata().pagingState : null;
-      this.executionInfo =
-          new DefaultExecutionInfo(
-              statement,
-              node,
-              startedSpeculativeExecutionsCount.get(),
-              executionIndex,
-              errors,
-              pagingState,
-              response,
-              true,
-              session,
-              context,
-              executionProfile);
-      return executionInfo;
+      return new DefaultExecutionInfo(
+          statement,
+          node,
+          startedSpeculativeExecutionsCount.get(),
+          executionIndex,
+          errors,
+          pagingState,
+          response,
+          true,
+          session,
+          context,
+          executionProfile);
     }
 
     private void logTimeoutSchedulingError(IllegalStateException timeoutError) {
